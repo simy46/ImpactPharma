97e5da931281aed932326b02348ba9af5eeeff8f
simy46 <missipsa.annane@polymtl.ca>
2025-07-18 15:33:26 -0400

    using pdfplumber for better text format

--- core/pdf_loader.py
+++ core/pdf_loader.py
@@ -1,11 +1,23 @@
-import PyPDF2
+# import PyPDF2
+
+# class PDFLoader:
+#     @staticmethod
+#     def extract_text(pdf_path: str) -> str:
+#         text = ""
+#         with open(pdf_path, "rb") as file:
+#             reader = PyPDF2.PdfReader(file)
+#             for page in reader.pages:
+#                 text += page.extract_text() + "\n"
+#         return text.strip()
+import pdfplumber
 
 class PDFLoader:
     @staticmethod
     def extract_text(pdf_path: str) -> str:
         text = ""
-        with open(pdf_path, "rb") as file:
-            reader = PyPDF2.PdfReader(file)
-            for page in reader.pages:
-                text += page.extract_text() + "\n"
+        with pdfplumber.open(pdf_path) as pdf:
+            for page in pdf.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text + "\n"
         return text.strip()
